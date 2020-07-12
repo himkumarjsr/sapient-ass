b22458a179c792f27b4065aa1e11a12eb5b261f9
Himanshu kumar <himanshukumar@MacBook-Pro.local>
2020-07-13 01:29:17 +0530

    Updates

---
+++ precache-manifest.be21edc7def8bdb1a162b8fcf5fce8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdd8e26b605564f4fd2a45d49e154f6",
+    "url": "/sapient-ass/index.html"
+  },
+  {
+    "revision": "da418804a0ab59992190",
+    "url": "/sapient-ass/static/css/main.7296775f.chunk.css"
+  },
+  {
+    "revision": "9be8046e9d2bdceb1680",
+    "url": "/sapient-ass/static/js/2.39d602e7.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/sapient-ass/static/js/2.39d602e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da418804a0ab59992190",
+    "url": "/sapient-ass/static/js/main.3e50aa8d.chunk.js"
+  },
+  {
+    "revision": "eac8107787028b512a55",
+    "url": "/sapient-ass/static/js/runtime-main.ffce9266.js"
+  }
+]);
